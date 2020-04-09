b85539cfc0f8e00d8e1e0a35100e624c924d2565
Gordon Mitchell <gordymitch@gmail.com>
2020-04-09 22:20:47 +0100

    new method of speech recognition

---
+++ more-tests/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import MainContainer from './containers/MainContainer';
+
+function App() {
+  return (
+      <MainContainer />
+    );
+}
+
+export default App;
---
+++ more-tests/src/containers/MainContainer.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from 'react';
+import Speech from './Speech';
+
+class MainContainer extends Component {
+
+
+  render(){
+    return(
+      <Fragment>
+        <Speech />
+      </Fragment>
+    )
+  }
+
+
+}
+
+export default MainContainer;
